b301825a2e330997458ec97063cb5fd513aba37c
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2021-03-18 01:46:46 -0300

    Update API

--- src/controller/logic/CreateRegister.js
+++ src/controller/logic/CreateRegister.js
@@ -15,6 +15,30 @@ module.exports = class CreateRegister {
     }
 
     async createQuiz(quizObject) {
+        const author = quizObject.author;
+        const title = quizObject.title;
+        const questions = quizObject.questions;
+        
+        var sqlQuiz = `insert into quiz (author, title) values ('${author}', '${title}')`;
+        var sqlQuestion = "insert into question (title_quiz, ask, a, b, c, right_letter) values "; 
+        var i = 0;
+        
+        for (; i < questions.length; i++) {
+            if (i === questions.length - 1)
+                sqlQuestion += `('${title}', '${questions[i].ask}', '${questions[i].a}', '${questions[i].b}', '${questions[i].c}', '${questions[i].right}')`;
+            else 
+                sqlQuestion += `('${title}', '${questions[i].ask}', '${questions[i].a}', '${questions[i].b}', '${questions[i].c}', '${questions[i].right}'), `;
+        }
+        
+        console.log(`1º sql -> ${sqlQuiz}`);
+        console.log(`2º sql -> ${sqlQuestion}`);
 
+        try {
+            await this.conn.execute(sqlQuiz);
+            await this.conn.execute(sqlQuestion);
+            return true;
+        } catch (error) {
+            return false;
+        }
     }
 }
--- src/routes/quiz.js
+++ src/routes/quiz.js
@@ -22,40 +22,43 @@ router.get('/view/:title', async (req, res) => {
 });
 
 // PENDENTE
-router.post('/new_quiz/:quizObject', async (req, res) => {
+router.post('/new_quiz/', async (req, res) => {
     const quiz  = req.body.quiz;
+    console.log("JSON recebido:");
     console.log(quiz);
     
-    /*
-     *  JSON Object esperado:
-     *  
-     *  {
-     *      'author': 'xxxxxx',
-     *      'title': 'xxxxxxx',
-     *      'questions': [{
-     *                      'ask': 'xxxxx xxx xxxx?',
-     *                      'a': 'xxxxxxxxxxxxx',
-     *                      'b': 'xxxxxxxxxxxxx',
-     *                      'c': 'xxxxxxxxxxxxx',
-     *                      'right': 'b',                   
-     *                   },
-     *                   {
-     *                      'ask': 'xxxxx xxx xxxx?',
-     *                      'a': 'xxxxxxxxxxxxx',
-     *                      'b': 'xxxxxxxxxxxxx',
-     *                      'c': 'xxxxxxxxxxxxx',
-     *                      'right': 'a',                   
-     *                   },
-     *                   {
-     *                      'ask': 'xxxxx xxx xxxx?',
-     *                      'a': 'xxxxxxxxxxxxx',
-     *                      'b': 'xxxxxxxxxxxxx',
-     *                      'c': 'xxxxxxxxxxxxx',
-     *                      'right': 'c',                   
-     *                   }]
-     *  }
-     *   
-    */
+/*
+*  JSON Object esperado:
+{
+    "quiz": {
+        "author": "xxxxxx",
+        "title": "xxxxxxx",
+        "questions": [
+            {
+                "ask": "xxxxx xxx xxxx?",
+                "a": "xxxxxxxxxxxxx",
+                "b": "xxxxxxxxxxxxx",
+                "c": "xxxxxxxxxxxxx",
+                "right": "b"
+            },
+            {
+                "ask": "xxxxx xxx xxxx?",
+                "a": "xxxxxxxxxxxxx",
+                "b": "xxxxxxxxxxxxx",
+                "c": "xxxxxxxxxxxxx",
+                "right": "a"
+            },
+            {
+                "ask": "xxxxx xxx xxxx?",
+                "a": "xxxxxxxxxxxxx",
+                "b": "xxxxxxxxxxxxx",
+                "c": "xxxxxxxxxxxxx",
+                "right": "c"
+            }
+        ]
+    }
+} 
+*/
 
     const CreateRegister = require(path.resolve('src/controller/logic/CreateRegister.js'));
     const result = await new CreateRegister().createQuiz(quiz);
