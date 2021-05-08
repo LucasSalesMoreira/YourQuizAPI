7d7eb2960661e96bd6f18cce93097256071f4dc1
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2021-05-08 02:16:11 -0300

    Script para teste de request adicionado

---
+++ src/public/tests/requests.js
@@ -0,0 +1,40 @@
+const quiz = {
+    author: 'LucasSales',
+    title: 'XXXXXXX',
+    questions: [
+        {
+            ask: 'XXXXXXXXXXXXXX',
+            a: 'XXXXXXX',
+            b: 'XXXXXXX',
+            c: 'XXXXXXX',
+            right: 'b'
+        },
+        {
+            ask: 'XXXXXXXXXXXXXX',
+            a: 'XXXXXXX',
+            b: 'XXXXXXX',
+            c: 'XXXXXXX',
+            right: 'c'
+        },
+        {
+            ask: 'XXXXXXXXXXXXXX',
+            a: 'XXXXXXX',
+            b: 'XXXXXXX',
+            c: 'XXXXXXX',
+            right: 'a'
+        }
+    ]
+};
+
+$.ajax({
+    url: '/quiz/new_quiz',
+    method: 'POST',
+    dataType: 'json',
+    data: quiz,
+    success: function(response) {
+        console.log(`OK! => ${response}`);
+    },
+    error: function(error) {
+        console.log(error);
+    }
+});
